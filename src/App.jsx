ba3448616a20898997652b5026d69428543ae0ed
Gabriel Lawson <gabriel@Gabriels-MacBook-Pro.local>
2023-07-31 13:50:25 -0700

    Part2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import { puppyList } from './data.js'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
 
   return (
     <>
-     
+      <div>
+        {
+          puppies.map((puppy) => {
+            return <p>{puppy.name}</p>
+          })
+        }
+
+      </div>
+
     </>
   )
 }
